3dc35b28bc36d380e31dfcec7a189e6160e17498
lhp81 <luke@Macintosh.local>
2014-02-13 22:36:21 -0800

    deleted old code

--- myfizzbuzz.py
+++ myfizzbuzz.py
@@ -17,34 +17,3 @@ def fizzbuzz_extendible(num, user_dict):
         return user_dict[num] + 'FizzBuzz'
     else:
         return fizzbuzz(num)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def fizzbuzz_extendable(num, numbers):
-    if num != 0 and num not in numbers:
-        if num % 3 == 0 and num % 5 == 0:
-            return "FizzBuzz"
-        elif num % 3 == 0:
-            return "Fizz"
-        elif num % 5 == 0:
-            return "Buzz"
-        elif num % 7 == 0:
-            return "Sivv"
-        else:
-            return str(num) + ' neither fizzes nor buzzes'
-    else:
-        return 'quit trying to make something out of nothing'
